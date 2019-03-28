5e549758552274f5d50296d92e771c3060f16a82
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-28 23:07:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
-
+	if speed > 60
+		true
+	elsif speed < 40
+		true
+	else
+		false
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed > 60
+		true
+	elsif speed < 40
+		true
+	else
+		false
 end
-	
-
-
